ff368add0f18f3862aed96e0139a32048f4053fb
Misael Flores <aflorescastrejon@gmail.com>
2024-05-05 20:55:58 -0500

    Agregamos el proyecto boletines

---
+++ boletines/javascript/main.js
@@ -0,0 +1,32 @@
+
+// Obtener los elementos de la clase .close
+
+let links = document.querySelectorAll(".close");
+
+//Recorrerlos
+
+links.forEach(function(link){
+    
+    //Agregar un evento click a cada uno de ellos
+    link.addEventListener("click",function(ev){
+        ev.preventDefault();
+        let content = document.querySelector(".content");
+
+    //Quitarle las clases de animación que ya tiene
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+
+    //Agregar clases para animar su salida fadeOutUp
+        content.classList.add("animate__fadeOutUp");
+        content.classList.add("animate__animated");
+
+        setTimeout(function(){
+            location.href = "/";
+        },600);
+        //setInterval
+        
+
+        return false;
+    });
+});
+
